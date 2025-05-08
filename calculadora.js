519e539bbb03dcba52c77700a3c564902da91efb
Lucas Quiej <quiej7959@gmail.com>
2025-05-08 12:43:18 -0600

    Resultado Final

--- calculadora.js
+++ calculadora.js
@@ -1,69 +1,72 @@
-
 const body = document.querySelector('body');
 const input = document.querySelector('#display')
 const botones = Array.from(document.querySelectorAll('.btn'));
-//Declaración de variables 
-//numero va a mantener el numero que tengamos antes de colocar un signo
-//op mantendrá la operación completa, ejemplo 25+10-30
-let op = ''
+
 let numero = ''
-// Nos permite colocar el texto del btn en el value del input
-//'Función dentro de una función "closure"'
-// Collback, función que pasa dentro de un parámetro dentro de otra función
+let op = ''
 
 botones.forEach(btn => {
     btn.addEventListener('click', (event) => {
-        console.log(event)
-            calculadora(event.target.innerText)
-        
+        calculadora(event.target.innerText)
     })
 })
-
 
 body.addEventListener('keydown', (event) => {
     calculadora(event.key)
 })
 
-//permite que la función esperada sea el valor que vamos a colocar en el input
 const calculadora = (valor) => {
-  console.log(valor);
-  
     if (!isNaN(valor) || valor == '.') {
+
         input.value += valor
         numero += valor
-        nuevoNum += valor
-        console.log();
-            
-      }
-      else if(valor == '+' || valor == '-' || valor == '*' || valor == '/' || valor == '(' || valor == ')' ){
-        //123481+   
+
+    } else if (valor == '+' || valor == '-' || valor == '*' || valor == '/' || valor == '(' || valor == ')') {
+
         input.value = input.value + valor
-        //     123481+
-        op = numero != '' ? op  + parseFloat(numero) + valor : op + valor
+
+        op = numero != '' ? op + parseFloat(numero) + valor : op + valor
         numero = ''
     }
-    else if(valor == 'Enter' || valor == '='){       
-        let op3;
-        for(let i =0; i < op.length; i++)
-        {
-            if(op[i] == '(' && !isNaN(op[0])){
-                if(!"/*+-".includes(op[i-1])){
-                    op3 = op.slice(0, i).concat('*')
-                    op4 = op.slice(i)
-                    op = op3 + op4
+    else if (valor == 'Enter' || valor == '=') {
+        let op2 = '';
+        let op3 = '';
+        let contParAbierto = op.split('').filter(item => item == '(').length
+        let contParCerrado = op.split('').filter(item => item == ')').length
+        let contador = 0;
+
+        if (contParAbierto == contParCerrado) {
+            while (contador < op.length) {
+                if ((op[contador] == '(' && !isNaN(op[contador - 1]))) {
+                    op2 += '*' + op[contador];
+                } else if (((op[contador] == ')' && !isNaN(op[contador + 1]) && op[contador + 1] != undefined))) {
+                    op2 += op[contador] + '*';
                 }
+                else {
+                    op2 += op[contador]
+                }
+                contador++
             }
 
-        }        
-        
-        input.value = numero == '' ? eval(op) : eval(op+parseFloat(numero))
+        } else {
+            console.log('Expresión mal formada')
+        }
+
+        op2 = op2.replaceAll(')(', ')*(')
+
+        if (op2[op2.length - 1] == ')') {
+            op3 = op2 + '*' + parseFloat(numero)
+        } else {
+            op3 = op2 + parseFloat(numero)
+        }
+        console.log(op2, op3)
+        input.value = numero == '' ? eval(op2) : eval(op3)
         op = ''
+        op2 = ''
         numero = input.value
-    }
-    else if (valor == 'Escape' || valor == "C") {
+    } else if (valor == 'Escape' || valor == 'C') {
         input.value = ''
         op = ''
         numero = ''
     }
-  }
-//Funcion de alto orden
+}   
